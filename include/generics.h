e5cb723d79085133703ca5153f2f826ec7084596
Every <christiansantoslima21@gmail.com>
2024-04-16 01:21:23 -0300

    build: "upload file structure and test clion"

---
+++ include/generics.h
@@ -0,0 +1,10 @@
+//
+// Created by christian on 16/04/24.
+//
+
+#ifndef GENERICS_H
+#define GENERICS_H
+
+#include <stdio.h>
+
+#endif //GENERICS_H
---
+++ src/generics.c
@@ -0,0 +1,9 @@
+//
+// Created by christian on 16/04/24.
+//
+
+#include "generics.h"
+
+main() {
+    printf("hello world\n");
+}

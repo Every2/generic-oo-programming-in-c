7ae07e7a014514cfbbc7332082810f098d4469f8
Every <christiansantoslima21@gmail.com>
2024-04-17 16:19:23 -0300

    refactor: improve code organization

--- include/generics.h
+++ include/generics.h
@@ -38,46 +38,5 @@ typedef struct {
     };
 } T;
 
-static void println(T value) {
-    switch (value.type) {
-        case INT8:
-            printf("%d\n", value.int8);
-            break;
-        case INT16:
-            printf("%d\n", value.int16);
-            break;
-        case INT32:
-            printf("%d\n", value.int32);
-            break;
-        case U8:
-            printf("%u\n", value.u8);
-            break;
-        case U16:
-            printf("%u\n", value.u16);
-            break;
-        case U32:
-            printf("%u\n", value.u32);
-            break;
-        case U64:
-            printf("%llu\n", (unsigned long long) value.u64);
-            break;
-        case INT64:
-            printf("%lld\n", value.int64);
-            break;
-        case CHAR:
-            printf("%c\n", value.character);
-            break;
-        case FLOAT:
-            printf("%f\n", value.low_precison_float);
-            break;
-        case DOUBLE:
-            printf("%lf\n", value.double_precision_float);
-            break;
-        case STRING:
-            printf("%s\n", value.string);
-            break;
-        default:
-            printf("Unknown type\n");
-    }
-}
+void println(T value);
 #endif //GENERICS_H
--- src/generics.c
+++ src/generics.c
@@ -1,9 +1,44 @@
 #include "generics.h"
 
-int main() {
-    T a;
-    a.type = INT32;
-    a.int32 = 12;
-    println(a);
-    return 0;
+void println(T value) {
+    switch (value.type) {
+        case INT8:
+            printf("%d\n", value.int8);
+        break;
+        case INT16:
+            printf("%d\n", value.int16);
+        break;
+        case INT32:
+            printf("%d\n", value.int32);
+        break;
+        case U8:
+            printf("%u\n", value.u8);
+        break;
+        case U16:
+            printf("%u\n", value.u16);
+        break;
+        case U32:
+            printf("%u\n", value.u32);
+        break;
+        case U64:
+            printf("%llu\n", (unsigned long long) value.u64);
+        break;
+        case INT64:
+            printf("%lld\n", value.int64);
+        break;
+        case CHAR:
+            printf("%c\n", value.character);
+        break;
+        case FLOAT:
+            printf("%f\n", value.low_precison_float);
+        break;
+        case DOUBLE:
+            printf("%lf\n", value.double_precision_float);
+        break;
+        case STRING:
+            printf("%s\n", value.string);
+        break;
+        default:
+            printf("Unknown type\n");
+    }
 }
